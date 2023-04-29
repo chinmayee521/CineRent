93b3a03e9c7db51da4982605ac0af2e66ec651ed
chinmayee <ctaralkar@gmail.com>
2023-04-29 18:49:36 +0530

    Add Membership type to list of customers, added name column in MembershipTypes table

--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -1,10 +1,15 @@
-﻿namespace CineRent.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CineRent.Models
 {
     public class MembershipType
     {
         public byte Id { get; set; } //primary key
+        [Required]
+        public string Name { get; set; }
         public short SignupFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
+
     }
 }
