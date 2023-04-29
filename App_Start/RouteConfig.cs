9c7dec9620cb6072200c7c135db6efaf2226d6fd
chinmayee <ctaralkar@gmail.com>
2023-04-29 12:16:12 +0530

    Trying out attribute routing (movies/released/2015/08)

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace CineRent
@@ -12,6 +8,16 @@ namespace CineRent
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            /*//custom routing
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate" },
+                new { year = @"\d{4}", month = @"\d{2}" });*/
+
+            //attribute routing
+            routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 name: "Default",
--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -9,7 +9,31 @@ namespace CineRent.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Rapunzel" };
+
             return View(movie);
         }
+
+        //attribute routing
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        /*public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }*/
     }
 }
