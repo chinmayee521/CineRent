91b21381b7390791fdbdcd2a113742bb4cdd502d
chinmayee <ctaralkar@gmail.com>
2023-04-29 13:00:55 +0530

    Displaying list of customers and movies

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,7 +1,6 @@
-﻿using System;
+﻿using CineRent.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace CineRent.Controllers
@@ -9,9 +8,30 @@ namespace CineRent.Controllers
     public class CustomersController : Controller
     {
         // GET: Customers
-        public ActionResult Index()
+        public ViewResult Index()
         {
-            return View();
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Chinu" },
+                new Customer { Id = 2, Name = "Apu" }
+            };
         }
     }
 }
--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -8,13 +8,30 @@ namespace CineRent.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Rapunzel" },
+                new Movie { Id = 2, Name = "The Good Dinosaur" }
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Rapunzel" };
             var customers = new List<Customer>
             {
-                new Customer{ Name = "customer 1"},
-                new Customer{ Name = "customer 2"}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
@@ -26,7 +43,7 @@ namespace CineRent.Controllers
             return View(viewModel);
         }
 
-        //attribute routing
+        /*//attribute routing
 
         [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
@@ -34,7 +51,7 @@ namespace CineRent.Controllers
             return Content(year + "/" + month);
         }
 
-        /*public ActionResult Edit(int id)
+        public ActionResult Edit(int id)
         {
             return Content("id=" + id);
         }
